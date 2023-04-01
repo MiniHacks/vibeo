7330bf1bd11f69f86d1671c7dd3182390d68218c
minizou <gh.mini.zou@gmail.com>
2023-04-01 18:38:54 -0500

    🏐 Add springy markers on landing

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from "next";
 import { Box, Text, Image, Heading } from "@chakra-ui/react";
 import { useRouter } from "next/router";
+import { motion } from "framer-motion";
 import PageLayout from "../components/Layout/PageLayout";
 import Footer from "../components/Layout/Footer";
 import Card from "../components/Card";
 import Button from "../components/Button";
 import { useSignInWithProvider } from "../lib/hooks/useSignInWithProvider";
+import Boing from "../components/Boing";
 
 // FIXME: turn the imgs into a background img in a box
 // so that the cards no longer have to be positioned absolute
@@ -18,9 +20,27 @@ const Home: NextPage = () => {
   const login = () => {
     signInWithProvider().then(() => router.push("/dashboard"));
   };
+
+  const markers = [
+    { src: "marker4.svg", delay: 0 },
+    { src: "marker2.svg", delay: 0 },
+    { src: "marker3.svg", delay: 0.25 },
+    { src: "marker1.svg", delay: 0.5 },
+  ];
+
   return (
     <PageLayout title={"vibeo - your personal video repository"}>
-      <Image fit={"fill"} position={"absolute"} src={"bg.svg"} zIndex={"-2"} />
+      <Image
+        fit={"fill"}
+        position={"absolute"}
+        src={"bg-base.svg"}
+        zIndex={"-2"}
+      />
+      {markers.map((marker, i) => (
+        <Boing key={i} delayTime={marker.delay}>
+          <Image fit={"fill"} position={"absolute"} src={marker.src} zIndex={"-1"} />
+        </Boing>
+      ))}
       <Box flexDirection={"column"} minH={"2150px"} display={"flex"}>
         <Box userSelect={"none"} flex={"1"}>
           <Card
