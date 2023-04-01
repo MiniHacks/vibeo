7898afac641e83e6bd531729ec47784c351032a5
minizou <gh.mini.zou@gmail.com>
2023-03-31 23:47:37 -0500

    🎂 Fix eslinting

--- packages/frontend/components/Layout/Footer.tsx
+++ packages/frontend/components/Layout/Footer.tsx
@@ -7,16 +7,64 @@ const Footer = (): JSX.Element => {
       <Flex h={"160px"} bg={"black"} justifyContent={"center"}>
         <HStack justifyContent={"space-between"}>
           <VStack alignItems={"left"}>
-            <Text lineHeight={'100%'} m={0} p={0} fontSize='3xl' fontFamily="Patrick Hand" color="white">vibeo</Text>
-            <Text letterSpacing={'0.05em'} lineHeight={'100%'} m={0} p={0} fontFamily="Poppins" fontWeight="semibold" color="white">
-              by <Text as="span" color={'brand.100'}>samyok</Text>, <Text as="span" color="brand.100">mini</Text>, <Text as="span" color="brand.100">sasha</Text>, and <Text as="span" color="brand.100">jack</Text>
+            <Text
+              lineHeight={"100%"}
+              m={0}
+              p={0}
+              fontSize={"3xl"}
+              fontFamily={"Patrick Hand"}
+              color={"white"}
+            >
+              vibeo
             </Text>
-            <Flex align="center" direction="column" justify="center" fontSize='xs' letterSpacing={'0.18em'} fontFamily={"Poppins"} color="white">
-              <Text>
-                <Link href='https://github.com/MiniHacks/vibeo'>github</Link>{' | '} 
-                <Link href='#'>devpost</Link>
+            <Text
+              letterSpacing={"0.05em"}
+              lineHeight={"100%"}
+              m={0}
+              p={0}
+              fontFamily={"Poppins"}
+              fontWeight={"semibold"}
+              color={"white"}
+            >
+              by{" "}
+              <Text as={"span"} color={"red"}>
+                samyok
               </Text>
-              <Text fontWeight="thin" fontSize='xs' fontFamily={"Poppins"} color="white">made with vibes for hackprinceton 2023</Text>
+              ,{" "}
+              <Text as={"span"} color={"brand.100"}>
+                mini
+              </Text>
+              ,{" "}
+              <Text as={"span"} color={"brand.100"}>
+                sasha
+              </Text>
+              , and{" "}
+              <Text as={"span"} color={"brand.100"}>
+                jack
+              </Text>
+            </Text>
+            <Flex
+              align={"center"}
+              direction={"column"}
+              justify={"center"}
+              fontSize={"xs"}
+              letterSpacing={"0.18em"}
+              fontFamily={"Poppins"}
+              color={"white"}
+            >
+              <Text>
+                <Link href={"https://github.com/MiniHacks/vibeo"}>github</Link>
+                {" | "}
+                <Link href={"#"}>devpost</Link>
+              </Text>
+              <Text
+                fontWeight={"thin"}
+                fontSize={"xs"}
+                fontFamily={"Poppins"}
+                color={"white"}
+              >
+                made with vibes for hackprinceton 2023
+              </Text>
             </Flex>
           </VStack>
         </HStack>
