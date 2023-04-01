26b3e1d1d9cbb8fe2a50ff28654ea88ec666e74a
Samyok Nepal <sam@yok.dev>
2023-04-01 00:12:47 -0400

    💛 yellow bkg

--- packages/frontend/theme.ts
+++ packages/frontend/theme.ts
@@ -2,4 +2,12 @@ import { extendTheme } from "@chakra-ui/react";
 
 export default extendTheme({
   initialColorMode: "light",
+  // body background yellow:
+  styles: {
+    global: () => ({
+      body: {
+        bg: "#FFC700",
+      },
+    }),
+  },
 });
