d317b235b5ae14ca19189d24f8553901f82ba013
minizou <gh.mini.zou@gmail.com>
2023-04-01 01:16:52 -0500

    🌇 Add background

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,13 +1,29 @@
 import type { NextPage } from "next";
-import { Box, Heading } from "@chakra-ui/react";
+import { Image, Box } from "@chakra-ui/react";
 import PageLayout from "../components/Layout/PageLayout";
 import Footer from "../components/Layout/Footer";
+import Card from "../components/Card";
 
 const Home: NextPage = () => {
   return (
     <PageLayout title={"geese, by minihacks"}>
-      <Box px={[5, 10]}>
-        <Heading as={"h1"}>geese</Heading>
+      <Box flexDirection={"column"} minH={"100vh"} display={"flex"}> 
+        <Box flex={"1"}>
+          <Image 
+            fit={"fill"}
+            src={"bg-marker.png"}
+            style={{
+              userSelect: "none",
+              position: "absolute",
+              zIndex: "1"
+            }}
+          />
+          <Image
+            fit={"fill"}
+            src={"bg-base.png"}
+            zIndex={"0"}
+          />
+        </Box>
       </Box>
       <Footer />
     </PageLayout>
