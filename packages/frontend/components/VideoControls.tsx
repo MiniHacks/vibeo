111dd18632de39d8c87e39f684075985b5083bcf
Jack Lee <welsar55@gmail.com>
2023-04-01 04:59:01 -0400

    😢 Broken Video page

--- packages/frontend/components/VideoControls.tsx
+++ packages/frontend/components/VideoControls.tsx
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 import { useState, useEffect } from "react";
 
 export type VideoControlsUser = {
+  id: string;
   color: string;
   iconUrl: string;
   progress: number;
@@ -17,18 +18,21 @@ export type VideoControlsProps = {
 
 const defaultUsers: VideoControlsUser[] = [
   {
+    id: "1",
     color: "red",
     iconUrl:
       "https://www.seattleaquarium.org/sites/default/files/images/animal/harborseal/Casey.jpg",
     progress: 0.2,
   },
   {
+    id: "2",
     color: "blue",
     iconUrl:
       "https://media.wired.co.uk/photos/60c8730fa81eb7f50b44037e/16:9/w_2560%2Cc_limit/1521-WIRED-Cat.jpeg",
     progress: 0.5,
   },
   {
+    id: "3",
     color: "green",
     iconUrl:
       "https://www.thesafaricollection.com/wp-content/uploads/2022/07/The-Safari-Collection-Hey-You-Giraffe-Manor-1.jpg",
@@ -43,6 +47,7 @@ const VideoControls = ({
 }: VideoControlsProps): JSX.Element => {
   const [progress, setProgress] = useState(0.4);
   const [isDragging, setIsDragging] = useState(false);
+  const [isPaused, setPaused] = useState(false);
 
   useEffect(() => {
     const updateProgress = () => {
@@ -105,14 +110,12 @@ const VideoControls = ({
     }
   };
 
-  const [isPaused, setPaused] = useState(false);
-
   const handlePauseClick = () => {
     if (isPaused) {
-      videoRef!.current!.play();
+      videoRef?.current?.play();
       setPaused(false);
     } else {
-      videoRef!.current!.pause();
+      videoRef?.current?.pause();
       setPaused(true); // TODO: Update this to true at the end of the video
     }
   };
@@ -126,9 +129,9 @@ const VideoControls = ({
   const [duration, setDuration] = useState(0);
 
   useEffect(() => {
-    const { duration } = videoRef?.current || {};
-    if (duration) {
-      setDuration(duration);
+    const newDuration = videoRef?.current?.duration || null;
+    if (newDuration) {
+      setDuration(newDuration);
     }
   }, [videoRef]);
 
@@ -138,8 +141,7 @@ const VideoControls = ({
 
   return (
     <Box
-      position={"absolute"}
-      w={"1000px"} // TODO: Probably change this to 100%
+      w={"unset"}
       bg={"#FFFFFF"}
       border={"4px solid #000000"}
       boxShadow={"-8px 10px 0px #000000"}
@@ -150,19 +152,27 @@ const VideoControls = ({
         <Text fontSize={"4xl"} fontWeight={"bold"}>
           {videoTitle || "Video"}
         </Text>
-        {users.map((user, index) => (
-          <Box
-            position="absolute"
-            right={`${(index + 1) * 25}px`}
-            width="50px" // TODO: Scale this with the font size of the header probably
-            height="50px"
-            borderRadius="50%"
-            border={`2px solid ${user.color}`}
-            overflow="hidden"
-          >
-            <Image src={user.iconUrl} alt={`User ${index + 1}`} w={"100%"} h={"100%"} />
-          </Box>
-        ))}
+        <Box position={"relative"}>
+          {users.map((user, index) => (
+            <Box
+              key={user.id}
+              position={"absolute"}
+              right={`${(index + 1) * 25}px`}
+              width={"50px"} // TODO: Scale this with the font size of the header probably
+              height={"50px"}
+              borderRadius={"50%"}
+              border={`2px solid ${user.color}`}
+              overflow={"hidden"}
+            >
+              <Image
+                src={user.iconUrl}
+                alt={`User ${index + 1}`}
+                w={"100%"}
+                h={"100%"}
+              />
+            </Box>
+          ))}
+        </Box>
       </Box>
       <Box display={"flex"} flexDirection={"row"}>
         <Box
@@ -178,32 +188,36 @@ const VideoControls = ({
           >
             {isPaused ? (
               <motion.svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="18"
-                height="18"
-                viewBox="0 0 18 18"
-                initial="hidden"
-                animate="visible"
-                exit="hidden"
+                xmlns={"http://www.w3.org/2000/svg"}
+                width={"18"}
+                height={"18"}
+                viewBox={"0 0 18 18"}
+                initial={"hidden"}
+                animate={"visible"}
+                exit={"hidden"}
               >
                 <path
-                  d="M9 0C4.0275 0 0 4.0275 0 9C0 13.9725 4.0275 18 9 18C13.9725 18 18 13.9725 18 9C18 4.0275 13.9725 0 9 0ZM6.75 4.5L13.5 9L6.75 13.5V4.5Z"
-                  fill="black"
+                  d={
+                    "M9 0C4.0275 0 0 4.0275 0 9C0 13.9725 4.0275 18 9 18C13.9725 18 18 13.9725 18 9C18 4.0275 13.9725 0 9 0ZM6.75 4.5L13.5 9L6.75 13.5V4.5Z"
+                  }
+                  fill={"black"}
                 />
               </motion.svg>
             ) : (
               <motion.svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="18"
-                height="18"
-                viewBox="0 0 18 18"
-                initial="hidden"
-                animate="visible"
-                exit="hidden"
+                xmlns={"http://www.w3.org/2000/svg"}
+                width={"18"}
+                height={"18"}
+                viewBox={"0 0 18 18"}
+                initial={"hidden"}
+                animate={"visible"}
+                exit={"hidden"}
               >
                 <path
-                  d="M10.5716 15.4265V3.29898C10.5716 2.63196 10.8129 2.06076 11.2955 1.58536C11.7781 1.10996 12.3576 0.872668 13.0339 0.873476H15.4961C16.1732 0.873476 16.7531 1.11118 17.2357 1.58657C17.7183 2.06197 17.9592 2.63277 17.9584 3.29898V15.4265C17.9584 16.0935 17.7171 16.6647 17.2345 17.1401C16.7519 17.6155 16.1724 17.8528 15.4961 17.852H13.0339C12.3567 17.852 11.7769 17.6143 11.2943 17.1389C10.8117 16.6635 10.5708 16.0927 10.5716 15.4265ZM0.722597 15.4265V3.29898C0.722597 2.63196 0.963898 2.06076 1.4465 1.58536C1.9291 1.10996 2.50855 0.872668 3.18485 0.873476H5.6471C6.32422 0.873476 6.90408 1.11118 7.38668 1.58657C7.86928 2.06197 8.11017 2.63277 8.10935 3.29898V15.4265C8.10935 16.0935 7.86805 16.6647 7.38545 17.1401C6.90285 17.6155 6.3234 17.8528 5.6471 17.852H3.18485C2.50773 17.852 1.92787 17.6143 1.44527 17.1389C0.962667 16.6635 0.721777 16.0927 0.722597 15.4265ZM13.0339 15.4265H15.4961V3.29898H13.0339V15.4265ZM3.18485 15.4265H5.6471V3.29898H3.18485V15.4265Z"
-                  fill="black"
+                  d={
+                    "M10.5716 15.4265V3.29898C10.5716 2.63196 10.8129 2.06076 11.2955 1.58536C11.7781 1.10996 12.3576 0.872668 13.0339 0.873476H15.4961C16.1732 0.873476 16.7531 1.11118 17.2357 1.58657C17.7183 2.06197 17.9592 2.63277 17.9584 3.29898V15.4265C17.9584 16.0935 17.7171 16.6647 17.2345 17.1401C16.7519 17.6155 16.1724 17.8528 15.4961 17.852H13.0339C12.3567 17.852 11.7769 17.6143 11.2943 17.1389C10.8117 16.6635 10.5708 16.0927 10.5716 15.4265ZM0.722597 15.4265V3.29898C0.722597 2.63196 0.963898 2.06076 1.4465 1.58536C1.9291 1.10996 2.50855 0.872668 3.18485 0.873476H5.6471C6.32422 0.873476 6.90408 1.11118 7.38668 1.58657C7.86928 2.06197 8.11017 2.63277 8.10935 3.29898V15.4265C8.10935 16.0935 7.86805 16.6647 7.38545 17.1401C6.90285 17.6155 6.3234 17.8528 5.6471 17.852H3.18485C2.50773 17.852 1.92787 17.6143 1.44527 17.1389C0.962667 16.6635 0.721777 16.0927 0.722597 15.4265ZM13.0339 15.4265H15.4961V3.29898H13.0339V15.4265ZM3.18485 15.4265H5.6471V3.29898H3.18485V15.4265Z"
+                  }
+                  fill={"black"}
                 />
               </motion.svg>
             )}
@@ -223,9 +237,9 @@ const VideoControls = ({
             justifyContent={"center"}
           >
             <Box bg={"#C4C4C4"} h={"2px"} w={"100%"} borderRadius={"2px"}>
-              {users.map((user, index) => (
+              {users.map((user) => (
                 <Box
-                  key={index}
+                  key={user.id}
                   bg={user.color}
                   h={"10px"}
                   w={"10px"}
