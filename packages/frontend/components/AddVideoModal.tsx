ffbacf93b8629ee405c52b6eeb33afa1b09eae53
Jack Lee <welsar55@gmail.com>
2023-04-01 17:02:15 -0400

    🐱‍👓 Make fetch request

--- packages/frontend/components/AddVideoModal.tsx
+++ packages/frontend/components/AddVideoModal.tsx
@@ -7,35 +7,51 @@ import {
   ModalOverlay,
   VStack,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 import { CardProps } from "./Card";
 
 const AddVideoModal = ({
+  uid,
   open,
   onClose,
 }: {
   open: boolean;
   onClose: () => void;
 }): JSX.Element => {
+  const [youtubeLink, setYoutubeLink] = useState("");
+
+  const handleYoutubeLinkChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setYoutubeLink(event.target.value);
+  };
+
+  const uploadYoutube = () => {
+    const url = `https://backend.vibeo.video/download?uid=${uid}&url=${youtubeLink}`;
+    fetch(url);
+    onClose();
+  };
+
   return (
     <Modal isOpen={open} onClose={onClose}>
       <ModalOverlay />
       <ModalContent p={8} {...CardProps}>
         <VStack spacing={8}>
           <HStack spacing={4}>
-            <Button onClick={onClose}>Upload</Button>
+            <Button onClick={uploadYoutube}>Upload</Button>
             <Divider orientation={"vertical"} />
             <Button onClick={onClose}>Record</Button>
           </HStack>
           <Input
             placeholder={"Youtube Link"}
             border={"2px solid black !important"}
+            value={youtubeLink}
+            onChange={handleYoutubeLinkChange}
           />
         </VStack>
       </ModalContent>
     </Modal>
   );
 };
-
 export default AddVideoModal;
