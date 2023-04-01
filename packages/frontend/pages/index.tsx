dc5b80601619e913a41ef16512acf2ad12800597
Jack Lee <welsar55@gmail.com>
2023-04-01 00:57:38 -0400

    😡 lint merge

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,13 +1,20 @@
 import type { NextPage } from "next";
 import { Box, Heading } from "@chakra-ui/react";
+import { useRef } from "react";
 import PageLayout from "../components/Layout/PageLayout";
 import Footer from "../components/Layout/Footer";
+import VideoPlayer, { VideoPlayerProps } from "../components/VideoPlayer";
+import VideoControls from "../components/VideoControls";
 
 const Home: NextPage = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
   return (
     <PageLayout title={"geese, by minihacks"}>
       <Box px={[5, 10]}>
         <Heading as={"h1"}>geese</Heading>
+        <VideoPlayer ref={videoRef} />
+        <VideoControls videoRef={videoRef} />
       </Box>
       <Footer />
     </PageLayout>
