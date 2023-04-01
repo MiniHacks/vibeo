edecc9b5a4f6f382c17d87cdc76afa7f5ce98ffa
Samyok Nepal <sam@yok.dev>
2023-04-01 09:52:01 -0400

    🤨 login/logout

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,15 +1,23 @@
 import type { NextPage } from "next";
 import { Box, Text, Image, Heading } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 import PageLayout from "../components/Layout/PageLayout";
 import Footer from "../components/Layout/Footer";
 import Card from "../components/Card";
 import Button from "../components/Button";
+import { useSignInWithProvider } from "../lib/hooks/useSignInWithProvider";
 
 // FIXME: turn the imgs into a background img in a box
 // so that the cards no longer have to be positioned absolute
 // and then parallax can be implemented
 
 const Home: NextPage = () => {
+  const [signInWithProvider] = useSignInWithProvider();
+
+  const router = useRouter();
+  const login = () => {
+    signInWithProvider().then(() => router.push("/dashboard"));
+  };
   return (
     <PageLayout title={"vibeo - your personal video repository"}>
       <Image fit={"fill"} position={"absolute"} src={"bg.svg"} zIndex={"-2"} />
@@ -28,7 +36,7 @@ const Home: NextPage = () => {
             <Text mb={"2em"} fontWeight={"light"} fontSize={"3xl"}>
               your shareable vault for everything audio and video
             </Text>
-            <Button fontSize={"3xl"} py={3} px={12}>
+            <Button fontSize={"3xl"} py={3} px={12} onClick={login}>
               Login
             </Button>
           </Card>
