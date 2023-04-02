8cee6e796ba602219451f51b8b8bf6e46286252c
minizou <gh.mini.zou@gmail.com>
2023-04-01 22:20:57 -0500

    🎞Add spacing bonanza to search modal

--- packages/frontend/components/SearchBar.tsx
+++ packages/frontend/components/SearchBar.tsx
@@ -42,6 +42,29 @@ export default function SearchBar({
   };
 
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const videos = [
+    {
+      src: "https://via.placeholder.com/400x300",
+      alt: "placeholder",
+      title: "llm time",
+      timestamp: "XX:XX",
+      context: "sasha makes a friend",
+    },
+    {
+      src: "https://via.placeholder.com/400x300",
+      alt: "placeholder",
+      title: "jack debut episode",
+      timestamp: "XX:XX",
+      context: "there's a hottub",
+    },
+    {
+      src: "https://via.placeholder.com/400x300",
+      alt: "placeholder",
+      title: "cookie mukbang",
+      timestamp: "XX:XX",
+      context: "samyok ate my cookie",
+    },
+  ];
 
   return (
     <>
@@ -77,7 +100,7 @@ export default function SearchBar({
           borderRadius={"lg"}
           minW={"50%"}
         >
-          <InputGroup p={0}>
+          <InputGroup>
             <Input
               type={"text"}
               onChange={handleChange}
@@ -96,28 +119,33 @@ export default function SearchBar({
               <Image alt={"magnifying glass search icon"} src={"search.svg"} />
             </InputRightElement>
           </InputGroup>
-          <Divider my={2} />
-          <div>
+          <Divider my={4} />
+          <Text fontSize={"xl"}>
             Quote from some video. Something something we are really cool
             people.
-          </div>
-          <div>↪ [XX:XX] of Video Title</div>
-          <Divider my={2} />
-
-          <Flex>
-            <Image
-              w={"20%"}
-              src={"https://via.placeholder.com/400x300"}
-              alt={"placeholder"}
-              borderRadius={"md"}
-            />
-            <Flex direction={"column"}>
-              <Text>Video Title</Text>
-              <Text>
-                Insert quote or context of where the searched term appears here.
-              </Text>
+          </Text>
+          <Text fontSize={"xl"}>↪ [XX:XX] of Video Title</Text>
+          <Divider my={6} />
+          {videos.map((video, i) => (
+            <Flex key={i} my={2} >
+              <Image
+                fill={"co"}
+                w={"20%"}
+                src={video.src}
+                alt={video.alt}
+                borderRadius={"md"}
+                objectFit={"cover"}
+              />
+              <Flex direction={"column"} ml={4}>
+                <Text fontWeight={"bold"} fontSize={"xl"}>
+                  {video.title}
+                </Text>
+                <Text>
+                  [{video.timestamp}] {video.context}
+                </Text>
+              </Flex>
             </Flex>
-          </Flex>
+          ))}
         </ModalContent>
       </Modal>
     </>
